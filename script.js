698801a6ca6f489b32b0aae1a126fe36ee2d0d85
River Merrill <rivermerrill.me@gmail.com>
2020-10-08 22:05:32 -0700

    :egg:

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener('keydown', keyHandler, false);
+let pattern = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
+let current = 0;
+
+function keyHandler (event) {
+	// If the key isn't in the pattern, or isn't the current key in the pattern, reset
+	if (pattern.indexOf(event.key) < 0 || event.key !== pattern[current]) {
+		current = 0;
+		return;
+	}
+
+	// Update how much of the pattern is complete
+	current++;
+
+	// If complete, alert and reset
+	if (pattern.length === current) {
+        current = 0;
+        alert('🍻')
+	}
+};
